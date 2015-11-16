9fcf0993e3f766509b8a1730878193fd604d7918
root <root@raspberrypi.(none)>
2015-11-16 16:12:34 -0600

    halfway done

--- hw3.s
+++ hw3.s
@@ -15,23 +15,28 @@ count_part:
 	PUSH {LR}
 	CMP R1,#0
 	MOVEQ R0,#1
+	POPEQ {PC}
 	MOVLT R0,#0
+	POPLT  {PC}
 	CMP R2,#0
 	MOVEQ R0,#0
-	BL _magic
-	POP {LR}
-	MOV PC,LR
-_magic:
-	PUSH {LR}
-	MOV R3,R1
-	SUB R1,R3,R2
+	POPEQ {PC}
+	PUSH {R0}
+	PUSH {R1}
+	SUB R1,R1,R2
+	PUSH {R0}
 	BL count_part
 	MOV R4,R0
-	MOV R1,R3
+	POP {R1}
+	PUSH {R2}
 	SUB R2,R2,#1
 	BL count_part
-	POP {LR}
-	MOV PC,LR
+	MOV R5,R0
+	ADD R0,R4,R5
+	POP {R2}
+	PUSH {R0}
+	POP {PC}
+	
 
 _scanf:
 	PUSH {LR}
@@ -41,16 +46,16 @@ _scanf:
 	BL scanf
 	LDR R0,[SP]
 	ADD SP,SP,#4
-	POP {LR}
-	MOV PC,LR
+	POP {PC}
+	
 
 _printf:
 	PUSH {LR}
 	LDR R0,=printf_str
 	MOV R1,R1
 	BL printf
-	POP {LR}
-	MOV PC,LR
+	POP {PC}
+
 
 
 .data
