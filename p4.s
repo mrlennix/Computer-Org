8d01fbdde266cbdc5f073f9f53f772b2f05cbe87
ChunkyPanda <neil.malhotra0@gmail.com>
2015-12-06 22:33:39 -0600

    Gay

--- p4.s
+++ p4.s
@@ -8,29 +8,28 @@ main:
 	BL _scanf
 	VMOV S1,R0
 	BL _divide
-	BL main
 _divide:
 	VDIV.F32 S2,S0,S1
 	VCVT.F64.F32 D4,S2
 	VMOV R1,R2,D4
 	BL _printf_result
+	B main
 	
 _scanf:
 	PUSH {LR}
-	PUSH {R1}
 	SUB SP,SP,#4
 	LDR R0,=format_str
 	MOV R1,SP
 	BL scanf
 	LDR R0,[SP]
 	ADD SP,SP,#4
-	POP {R1}
 	POP {PC}
 _printf_result:
 	PUSH {LR}
 	LDR R0, =result_str
 	BL printf
 	POP {PC}
+	
 
 
 .data
